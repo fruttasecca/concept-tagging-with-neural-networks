7838d25c12371faf7836522e8d64d43d9977e99a
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2018-07-27 16:05:44 +0200

    added svm

---
+++ src/svm/atis/exec.sh
@@ -0,0 +1,5 @@
+make CORPUS=exp.train_dev_bin6.txt MODEL=train  SVM_PARAM="-t1 -d1 -s1.235739 -r1.444492 -c0.401410 -m 8000" FEATURE="F:-6..4:0..0 F:-1..0:1..2 F:0..0:0.. T:-2..-1" train
+yamcha -m train.model < exp.test.txt | expand -t 1 > svm.txt
+rm train*
+
+
