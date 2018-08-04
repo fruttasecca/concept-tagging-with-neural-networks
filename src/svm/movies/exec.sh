80efacbcc88dd8a41efd3dca06627035ad3b6c4d
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2018-08-04 11:26:19 +0200

    refactored svms and moved their input files in data

--- src/svm/movies/exec.sh
+++ src/svm/movies/exec.sh
@@ -1,3 +1,3 @@
-make CORPUS=exp.train.data MODEL=train  SVM_PARAM="-t1 -d2 -s1.868520 -r1.964100 -c1.421407 -m 8000 " FEATURE="F:-4..4:0..0 F:-1..0:2..3 F:0..0:0.. T:-2..-1" train
-yamcha -m train.model < exp.test.data | expand -t 1 > svm.txt
+make CORPUS=$1 MODEL=train  SVM_PARAM="-t1 -d2 -s1.868520 -r1.964100 -c1.421407 -m 8000 " FEATURE="F:-4..4:0..0 F:-1..0:2..3 F:0..0:0.. T:-2..-1" train
+yamcha -m train.model < $2 | expand -t 1 > svm.txt
 rm train*
